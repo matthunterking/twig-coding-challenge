e1f157e293c51d418684b9d976c99195d6c97571
matthunterking <matthew85king@gmail.com>
2021-11-04 11:53:36 +0000

    Set up test cases

--- test.js
+++ test.js
@@ -2,8 +2,25 @@
 const { expect } = require('chai');
 const { groupArrayElements } = require('./index.js');
 
-describe('test', () => {
-  it('should test correctly', () => {
-    expect(groupArrayElements()).to.equal(true);
+const testCases = [
+  { args: [[1], 1], resultArray: [[1]] },
+  { args: [[1, 2], 1], resultArray: [[1, 2]] },
+  { args: [[1, 2], 2], resultArray: [[1], [2]] },
+  { args: [[1, 2, 3], 2], resultArray: [[1, 2], [3]] },
+  { args: [[1, 2, 3, 4, 5], 3], resultArray: [[1, 2], [3, 4], [5]] },
+  { args: [[1, 2, 3, 4, 5, 6], 3], resultArray: [[1, 2], [3, 4], [5, 6]] },
+  { args: [[1, 2, 3, 4, 5, 6], 2], resultArray: [[1, 2, 3], [4, 5, 6]] }
+];
+
+const stringifyResult = (resultArray) => resultArray.reduce((string, array, index) => {
+  string = string + `[${array}]${index === (resultArray.length - 1) ? '' : ','}`;
+  return string;
+}, '');
+
+describe('groupArrayElements', () => {
+  testCases.forEach(({ args, resultArray }) => {
+    it(`should return [${stringifyResult(resultArray)}] when given an array of [${args[0]}] to be divided into ${args[1]} equally sized arrays`, () => {
+      expect(groupArrayElements(...args)).to.deep.equal(resultArray);
+    });
   });
 });
