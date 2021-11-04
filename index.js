719c33388b665bb2e60386c245107e115d7de0b8
matthunterking <matthew85king@gmail.com>
2021-11-04 12:05:41 +0000

    Added groupArrayElements function to index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const groupArrayElements = () => {
-  return true;
+const groupArrayElements = (array, numberOfNewArrays) => {
+  const sizeOfNewArrays = Math.ceil(array.length / numberOfNewArrays);
+  return new Array(numberOfNewArrays).fill('').map((_, index) => {
+    const startPoint = index * sizeOfNewArrays;
+    return array.slice(startPoint, startPoint + sizeOfNewArrays);
+  });
 };
 
 module.exports = { groupArrayElements };
